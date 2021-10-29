7a1844b1b1fddc1f775608617beca9b0e736c8ce
David Ford <d.ford@digital-velocity.co.uk>
2021-10-29 12:33:45 +0100

    Step 2 - Meta data and CSS/SCSS -  https://nextjs.org/learn/basics/assets-metadata-css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,24 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
+    const pageTitle = 'My First Post';
+    const seoTitle = pageTitle + ' | NextJs Tutorial';
   return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+    <Layout>
+        <Head>
+            <title>{ seoTitle }</title>
+            <meta property="og:title" content="{ seoTitle }" key="title" />
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <h1>{ pageTitle }</h1>
+        <h2>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+        </h2>
+    </Layout>
   )
 }
